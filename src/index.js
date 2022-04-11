669a68d8e90ae40de73990faaf4329bb1e493251
minJonRing <tuqiaorong@zhejian.com>
2022-04-11 17:21:26 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import InputPro from "../packages/InputPro/src/main.vue";
-import SelectPro from "../packages/SelectPro/src/main.vue";
-import CascaderPro from "../packages/CascaderPro/src/main.vue";
-import CheckPro from "../packages/CheckPro/src/main.vue";
-import DatePro from "../packages/DatePro/src/main.vue";
-import RadioPro from "../packages/RadioPro/src/main.vue";
-import SwitchPro from "../packages/SwitchPro/src/main.vue";
-import TimeSlotPro from "../packages/TimeSlotPro/src/main.vue";
-import FormPro from "../packages/FormPro/src/main.vue";
-import ListPro from "../packages/ListPro/src/main.vue";
-import StaticListPro from "../packages/StaticListPro/src/main.vue";
+// import SelectPro from "../packages/SelectPro/src/main.vue";
+// import CascaderPro from "../packages/CascaderPro/src/main.vue";
+// import CheckPro from "../packages/CheckPro/src/main.vue";
+// import DatePro from "../packages/DatePro/src/main.vue";
+// import RadioPro from "../packages/RadioPro/src/main.vue";
+// import SwitchPro from "../packages/SwitchPro/src/main.vue";
+// import TimeSlotPro from "../packages/TimeSlotPro/src/main.vue";
+// import FormPro from "../packages/FormPro/src/main.vue";
+// import ListPro from "../packages/ListPro/src/main.vue";
+// import StaticListPro from "../packages/StaticListPro/src/main.vue";
 
 const components = [
   InputPro,
-  SelectPro,
-  CascaderPro,
-  CheckPro,
-  DatePro,
-  RadioPro,
-  SwitchPro,
-  TimeSlotPro,
-  FormPro,
-  ListPro,
-  StaticListPro
+  // SelectPro,
+  // CascaderPro,
+  // CheckPro,
+  // DatePro,
+  // RadioPro,
+  // SwitchPro,
+  // TimeSlotPro,
+  // FormPro,
+  // ListPro,
+  // StaticListPro
 ]
 const install = function (Vue, opts = {}) {
   components.forEach(component => {
@@ -36,17 +36,17 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '0.1.0',
+  version: '0.1.6',
   install,
   InputPro,
-  SelectPro,
-  CascaderPro,
-  CheckPro,
-  DatePro,
-  RadioPro,
-  SwitchPro,
-  TimeSlotPro,
-  FormPro,
-  ListPro,
-  StaticListPro
+  // SelectPro,
+  // CascaderPro,
+  // CheckPro,
+  // DatePro,
+  // RadioPro,
+  // SwitchPro,
+  // TimeSlotPro,
+  // FormPro,
+  // ListPro,
+  // StaticListPro
 }
